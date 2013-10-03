0fec931799177a4a96c5401efc4c3f121053e4e4
robertbaptist <robert.baptist@live.nl>
2013-10-03 21:28:46 +0200

    Initial commit.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace MouseRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this._recordButton = new System.Windows.Forms.Button();
+            this._playButton = new System.Windows.Forms.Button();
+            this._stopButton = new System.Windows.Forms.Button();
+            this._recordTimer = new System.Windows.Forms.Timer(this.components);
+            this._playTimer = new System.Windows.Forms.Timer(this.components);
+            this._repeatCounter = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this._repeatCounter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _recordButton
+            // 
+            this._recordButton.Location = new System.Drawing.Point(12, 12);
+            this._recordButton.Name = "_recordButton";
+            this._recordButton.Size = new System.Drawing.Size(75, 75);
+            this._recordButton.TabIndex = 0;
+            this._recordButton.Text = "Record";
+            this._recordButton.UseVisualStyleBackColor = true;
+            this._recordButton.Click += new System.EventHandler(this._recordButton_Click);
+            // 
+            // _playButton
+            // 
+            this._playButton.Location = new System.Drawing.Point(174, 12);
+            this._playButton.Name = "_playButton";
+            this._playButton.Size = new System.Drawing.Size(75, 75);
+            this._playButton.TabIndex = 1;
+            this._playButton.Text = "Play";
+            this._playButton.UseVisualStyleBackColor = true;
+            this._playButton.Click += new System.EventHandler(this._playButton_Click);
+            // 
+            // _stopButton
+            // 
+            this._stopButton.Location = new System.Drawing.Point(93, 12);
+            this._stopButton.Name = "_stopButton";
+            this._stopButton.Size = new System.Drawing.Size(75, 75);
+            this._stopButton.TabIndex = 2;
+            this._stopButton.Text = "Stop";
+            this._stopButton.UseVisualStyleBackColor = true;
+            this._stopButton.Click += new System.EventHandler(this._stopButton_Click);
+            // 
+            // _recordTimer
+            // 
+            this._recordTimer.Interval = 10;
+            this._recordTimer.Tick += new System.EventHandler(this._recordTimer_Tick);
+            // 
+            // _playTimer
+            // 
+            this._playTimer.Interval = 10;
+            this._playTimer.Tick += new System.EventHandler(this._playTimer_Tick);
+            // 
+            // _repeatCounter
+            // 
+            this._repeatCounter.Location = new System.Drawing.Point(64, 94);
+            this._repeatCounter.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this._repeatCounter.Name = "_repeatCounter";
+            this._repeatCounter.Size = new System.Drawing.Size(185, 20);
+            this._repeatCounter.TabIndex = 3;
+            this._repeatCounter.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this._repeatCounter.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Repeat:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 127);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._repeatCounter);
+            this.Controls.Add(this._stopButton);
+            this.Controls.Add(this._playButton);
+            this.Controls.Add(this._recordButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this._repeatCounter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _recordButton;
+        private System.Windows.Forms.Button _playButton;
+        private System.Windows.Forms.Button _stopButton;
+        private System.Windows.Forms.Timer _recordTimer;
+        private System.Windows.Forms.Timer _playTimer;
+        private System.Windows.Forms.NumericUpDown _repeatCounter;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MouseRecorder
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        private const int MOUSEEVENT_MOVE = 0x01;
+        private const int MOUSEEVENT_LEFTDOWN = 0x02;
+        private const int MOUSEEVENT_LEFTUP = 0x04;
+        private const int MOUSEEVENT_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENT_RIGHTUP = 0x10;
+        private const int MOUSEEVENT_MIDDLEDOWN = 0x20;
+        private const int MOUSEEVENT_MIDDLEUP = 0x40;
+
+        private struct _MouseAction
+        {
+            public Point Position;
+            public MouseButtons Button;
+        }
+
+        private int _index = 0;
+        private int _count = 0;
+
+        private MouseButtons _currentButton;
+
+        private List<_MouseAction> _mouseActions = new List<_MouseAction>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            _playButton.Enabled = false;
+            _stopButton.Enabled = false;
+        }
+
+        private void _playButton_Click(object sender, EventArgs e)
+        {
+            _playButton.Enabled = false;
+            _recordButton.Enabled = false;
+            _count = 0;
+            _playTimer.Start();
+        }
+
+        private void _stopButton_Click(object sender, EventArgs e)
+        {
+            _recordButton.Enabled = true;
+            _recordTimer.Stop();
+            _playButton.Enabled = true;
+            _stopButton.Enabled = false;
+        }
+
+        private void _recordButton_Click(object sender, EventArgs e)
+        {
+            _recordButton.Enabled = false;
+            _mouseActions.Clear();
+            _recordTimer.Start();
+            _stopButton.Enabled = true;
+        }
+
+        private void _playTimer_Tick(object sender, EventArgs e)
+        {
+            if (_index < _mouseActions.Count)
+            {
+                _MouseAction mouseAction = _mouseActions[_index++];
+
+                Cursor.Position = mouseAction.Position;
+
+                if (_currentButton == MouseButtons.Left && mouseAction.Button != MouseButtons.Left)
+                {
+                    mouse_event(MOUSEEVENT_LEFTUP, (uint)mouseAction.Position.X, (uint)mouseAction.Position.Y, 0, 0);
+                }
+                else if (_currentButton == MouseButtons.Right && mouseAction.Button != MouseButtons.Right)
+                {
+                    mouse_event(MOUSEEVENT_RIGHTUP, (uint)mouseAction.Position.X, (uint)mouseAction.Position.Y, 0, 0);
+                }
+                else if (_currentButton == MouseButtons.Middle && mouseAction.Button != MouseButtons.Middle)
+                {
+                    mouse_event(MOUSEEVENT_MIDDLEUP, (uint)mouseAction.Position.X, (uint)mouseAction.Position.Y, 0, 0);
+                }
+
+                switch (mouseAction.Button)
+                {
+                    case MouseButtons.Left:
+                        if (_currentButton != MouseButtons.Left)
+                            mouse_event(MOUSEEVENT_LEFTDOWN, (uint)mouseAction.Position.X, (uint)mouseAction.Position.Y, 0, 0);
+                        break;
+
+                    case MouseButtons.Right:
+                        if (_currentButton != MouseButtons.Right)
+                            mouse_event(MOUSEEVENT_RIGHTDOWN, (uint)mouseAction.Position.X, (uint)mouseAction.Position.Y, 0, 0);
+                        break;
+
+                    case MouseButtons.Middle:
+                        if (_currentButton != MouseButtons.Middle)
+                            mouse_event(MOUSEEVENT_MIDDLEDOWN, (uint)mouseAction.Position.X, (uint)mouseAction.Position.Y, 0, 0);
+                        break;
+                }
+
+                _currentButton = mouseAction.Button;
+
+            }
+            else
+            {
+                _index = 0;
+                if (++_count == _repeatCounter.Value)
+                {
+                    _playButton.Enabled = true;
+                    _playTimer.Stop();
+                    _recordButton.Enabled = true;
+                }
+            }
+        }
+
+        private void _recordTimer_Tick(object sender, EventArgs e)
+        {
+            _mouseActions.Add(new _MouseAction { Position = new Point(Control.MousePosition.X, Control.MousePosition.Y), Button = Control.MouseButtons });
+        }
+    }
+}
